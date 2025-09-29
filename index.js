8d388adb9ecc29a0140c6ae3bb49f7e8a794a5f7
Adja Sira <adjasirad@gmail.com>
2025-09-29 12:33:27 +0200

    Refactor: define HTTP callback function separately

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// Import a module
 const http = require('http')
 
-// Declare an http server
-http.createServer(function (req, res) {
-
-  // Write a response header
+const serverHandle = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  // Write a response content
   res.end('Hello World\n')
+}
 
-// Start the server
-}).listen(8080)
+http.createServer(serverHandle).listen(8080)
