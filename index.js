9e765a1c459f044ab3124f9cf58fd7b26dcb65ee
Adja Sira <adjasirad@gmail.com>
2025-09-30 05:29:32 +0200

    refactor(server): extract request handler into separate module

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 const http = require('http')
-const url = require('url')
-const qs = require('querystring')
+const handles = require('./handles')
 
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end()
-}
-
-http.createServer(serverHandle).listen(8080)
+http.createServer(handles.serverHandle).listen(8080)
