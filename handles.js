80a841554851c3faccb7a4e224d09ccce9397b7c
Adja Sira <adjasirad@gmail.com>
2025-09-30 05:59:50 +0200

    feat(app): add multi-route server

--- handles.js
+++ handles.js
@@ -1,20 +1,69 @@
-const url = require("url");
-const qs = require("querystring");
+const url = require('url')
+const qs = require('querystring')
+const fs = require('fs')
+const pathModule = require('path')
 
 module.exports = {
   serverHandle: function (req, res) {
-    const route = url.parse(req.url);
-    const path = route.pathname;
-    const params = qs.parse(route.query);
+    const route = url.parse(req.url)
+    const pathname = route.pathname
+    const params = qs.parse(route.query)
 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-
-    if (path === "/hello" && "name" in params) {
-      res.write("Hello " + params["name"]);
-    } else {
-      res.write("Hello anonymous");
+    // Route: /
+    if (pathname === '/') {
+      const html = `
+        <!DOCTYPE html>
+        <html>
+          <head><meta charset="utf-8"><title>Home</title></head>
+          <body>
+            <h1>Welcome</h1>
+            <p>Try <a href="/hello?name=Adja">/hello?name=Adja</a></p>
+            <p>Or <a href="/about">/about</a></p>
+          </body>
+        </html>
+      `
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(html)
+      return
     }
 
-    res.end();
-  },
-};
+    // Route: /hello
+    if (pathname === '/hello') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      if (params.name === 'Adja') {
+        res.end('Hello Adja! You are a thoughtful and methodical learner passionate about ethical tech.')
+      } else if (params.name) {
+        res.end('Hello ' + params.name)
+      } else {
+        res.end('Hello anonymous')
+      }
+      return
+    }
+
+    // Route: /about
+    if (pathname === '/about') {
+      try {
+        const about = require('./content/about.json')
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(about, null, 2))
+      } catch (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end('Error loading about.json')
+      }
+      return
+    }
+
+    // Dynamic route: /content/filename
+    const contentPath = pathModule.join(__dirname, 'content', pathname.replace('/', '') + '.json')
+    if (fs.existsSync(contentPath)) {
+      const fileContent = fs.readFileSync(contentPath)
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(fileContent)
+      return
+    }
+
+    // Route not found
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('404 Not Found')
+  }
+}
