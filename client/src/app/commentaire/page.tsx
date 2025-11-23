e30734f38ff402d6419d85b8584412213bc4d3da
Yxssir <yassirmourns@gmail.com>
2025-11-23 19:23:51 +0100

    fix commentaire page by forcing client component

--- client/src/app/commentaire/page.tsx
+++ client/src/app/commentaire/page.tsx
@@ -1,17 +1,17 @@
+"use client";
+
 import CommentaireContent from "./CommentaireContent";
 
 export default function CommentairePage({
   searchParams,
-}: {
-  searchParams?: Record<string, string | string[] | undefined>;
-}) {
+}: any) {
   const menuId = searchParams?.menu_id;
   const menuTitle = searchParams?.menu_title;
 
   return (
     <CommentaireContent
-      menuId={menuId as string | undefined}
-      menuTitle={menuTitle as string | undefined}
+      menuId={menuId}
+      menuTitle={menuTitle}
     />
   );
 }
